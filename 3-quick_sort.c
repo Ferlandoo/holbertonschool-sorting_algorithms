a293042602a926c3f6e22a5f0b75537f2b209f2a
Ferlandoo <contact@ferlando.me>
2024-01-12 12:07:30 +0100

    update code

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -47,28 +47,27 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1;
-	int j, tmp;
+	int i = low - 1, j, temp;
 
 	for (j = low; j <= high - 1; j++)
 	{
-		if (array[j] < pivot)
+		if (array[j] <= pivot)
 		{
 			i++;
 			if (i != j)
 			{
-				tmp = array[i];
+				temp = array[i];
 				array[i] = array[j];
-				array[j] = tmp;
+				array[j] = temp;
 				print_array(array, size);
 			}
 		}
 	}
-	if (array[high] < array[i + 1])
+	if (i + 1 != high)
 	{
-		tmp = array[i + 1];
+		temp = array[i + 1];
 		array[i + 1] = array[high];
-		array[high] = tmp;
+		array[high] = temp;
 		print_array(array, size);
 	}
 	return (i + 1);
