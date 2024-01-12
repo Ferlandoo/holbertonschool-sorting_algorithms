7b938db24e9156ca02aeccd99670b33ce2ae0864
Ferlandoo <contact@ferlando.me>
2024-01-12 11:42:47 +0100

    add ex 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using the Quick
+ * sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quick_sort_recursive - Sorts an array of integers in ascending order using
+ * the Quick sort algorithm
+ * @array: The array to be sorted
+ * @low: The lowest index of the partition to be sorted
+ * @high: The highest index of the partition to be sorted
+ * @size: The size of the array
+ */
+
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot - 1, size);
+		quick_sort_recursive(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * partition - Lomuto partition scheme
+ * @array: The array to be sorted
+ * @low: The lowest index of the partition to be sorted
+ * @high: The highest index of the partition to be sorted
+ * @size: The size of the array
+ * Return: The index of the pivot
+ */
+
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, tmp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[high] < array[i + 1])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = tmp;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
